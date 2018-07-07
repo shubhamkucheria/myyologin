5f7bd42817c67a4337445d62aaed9c1fcc0504e5
shubhamkucheria <shubhamkucheria@gmail.com>
2018-07-07 22:17:51 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# myyologin
+
+MEAN Stack User Registration and Login
+To see a demo and further details go to
---
+++ app/map/index.controller.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .controller('Map.IndexController', Controller);
+
+    function Controller(UserService) {
+        var vm = this;
+
+        vm.user = null;
+
+        initController();
+
+        function initController() {
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+                console.log('->>>>>>>>>>>>>>>>>>>>>>>>>>>');
+                console.log( vm.user);
+
+            });
+        }
+    }
+
+})();
